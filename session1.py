f18db6cb78ec4c0209fdb15374cf4305eb7a102d
amirhossein hajitabar <amirhajitabar2@gmail.com>
2026-02-21 19:36:07 +0330

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,72 @@
+# print("hello")
+
+# username = "amir hossein"
+# score = 20
+
+# if = 5
+
+
+# underline
+# student_score = 20
+# student_first_name="amir"
+# student_last_name="hajitabar"
+
+# lowerCamelCase
+# studentScore = 20
+# studentFirstName="amir"
+# studentLastName="hajitabar"
+
+
+# name = input("please enter name: ")
+# print(name)
+
+# username = "amirhossein"
+# print(username)
+
+# print("amirhossein",end=" ")
+# print("hajitabar")
+# print("from babol")
+
+# username = "amir hossein"
+# print(type(username))
+# print(username)
+# print(username[1])
+# print(username[4])
+# print(username[-1])
+# print(username[1:7])
+# print(username[:7])
+# print(username[7:])
+# print(username[:])
+# print(username[2:7:2])
+# print(username[::-1]) #Reverse
+
+# name = "amir"
+# print(name.upper())
+
+# city = "BABOL"
+# print(city.lower())
+
+# message = "hello how are you?"
+# print(message.title())
+
+# student = "amir reza"
+# print(student.replace("r","m"))
+
+# products = "apple,xiaomi,samsung"
+# print(products.split(","))
+
+# user = " amir "
+# print(user.strip())
+
+# F-string
+# firstName = "amir"
+# lastName = "hajitabar"
+# score = 20
+
+# print(f"welcome mr {firstName} {lastName} with {score}")
+
+# print("welcome mr {} {} with {}".format(firstName,lastName,score))
+
+
+# print("amirhossein\nhajitabar")
+print("amirhossein\thajitabar")
