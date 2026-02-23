90bb62d2d1f8f14231dc55ba0cc399e6317df1c8
amirhossein hajitabar <amirhajitabar2@gmail.com>
2026-02-23 18:33:44 +0330

    Create Exercise1.py

---
+++ Exercise1.py
@@ -0,0 +1,42 @@
+# Question 1
+# date = "2025-04-12"
+# print (date.replace("-","/"))
+
+# updatedDate = date.replace("-","/")
+# print(updatedDate)
+
+# Question 2
+# a=int(input("please enter a: "))
+# b=int(input("please enter b: "))
+# c=int(input("please enter c: "))
+# x=int(input("please enter x: "))
+
+# y=a*(x**2)+(b*x)+c
+
+# print(y)
+
+# Question3 
+
+# name = input("please enter name: ")
+# pl1=input("please enter pl1: ")
+# pl2=input("please enter pl2: ")
+# pl3=input("please enter pl3: ")
+
+# print(f"my name is {name.upper()}\n\t1-{pl1}\n\t2-{pl2}\n\t3-{pl3}")
+
+# Question 4
+# a=int(input("please enter a: "))
+# b=int(input("please enter b: "))
+# c=int(input("please enter c: "))
+# d=int(input("please enter d: "))
+# e=int(input("please enter e: "))
+
+# print((a+b+c+d+e)/5)
+
+# Question 5
+
+degree = int(input("please enter degree: "))
+
+f = (degree * 1.8) + 32
+
+print(f)
